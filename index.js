7b9453f0b24dfed400c40d098058f471048d340b
VictoriaCarvalho <vix219@icloud.com>
2025-01-06 13:42:13 -0600

    add lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards() {
+  const messages = []; 
+  for (let i = 0; i < names.length; i++) {
+    const message = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+    console.log(message); 
+    messages.push(message); 
+  }
+  return messages; 
+}
+
+const thankYouMessages = writeCards(); 
+console.log(thankYouMessages);
+
+
+function countDown() {
+
+let countDown = 0;
+while (countDown < 11) 
+  console.log(countDown++);
+}
